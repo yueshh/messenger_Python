28cb85ce2cb8ae260ae7854eb8e9dca34c3603bb
yueshh <cool.tzfftzftzftzftz@yandex.ru>
2024-07-25 18:15:45 +0300

    123

--- hash_password.py
+++ hash_password.py
@@ -1,16 +1,16 @@
-import re
-
-
-class password_verification:
-    def __init__(self):
-        self.password = None
-
-    def comparison(self, password, rp_password):
-        if (re.findall(' ', password) and (password == rp_password)):
-
-
-
-
-    def __hash__(self):
-        return hash(self.password)
-
+# import re
+#
+#
+# class password_verification:
+#     def __init__(self):
+#         self.password = None
+#
+#     def comparison(self, password, rp_password):
+#         if (re.findall(' ', password) and (password == rp_password)):
+#
+#
+#
+#
+#     def __hash__(self):
+#         return hash(self.password)
+#
